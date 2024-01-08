2d6429f4c9eeaff7401d1004521914bb6a6ef8f8
Kobayashi Takahiro <koba925@gmail.com>
2024-01-08 14:59:46 +0900

    Scannerを実装

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,131 @@
 use std::io;
 
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
+    Semicolon,
+    Number(f64),
+    EOF,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Token {
+    tt: TokenType,
+    lexeme: String,
+}
+
+impl Token {
+    pub fn new(tt: TokenType, lexeme: String) -> Token {
+        Token { tt, lexeme }
+    }
+}
+
+#[derive(Debug)]
+pub struct Scanner {
+    src: Vec<char>,
+    start: usize,
+    current: usize,
+    tokens: Vec<Token>,
+    has_error: bool,
+}
+
+impl Scanner {
+    pub fn new(src: String) -> Scanner {
+        Scanner {
+            src: src.chars().collect(),
+            start: 0,
+            current: 0,
+            tokens: Vec::new(),
+            has_error: false,
+        }
+    }
+
+    pub fn scan(&mut self) {
+        self.skip_whitespaces();
+        while !self.at_end() {
+            if self.current_char() == ';' {
+                self.advance();
+                self.add_token(TokenType::Semicolon);
+                self.advance();
+            } else if self.current_char().is_ascii_digit() {
+                self.number();
+            } else {
+                eprintln!("Error: Unexpected char");
+                self.has_error = true;
+                return;
+            }
+            self.skip_whitespaces();
+        }
+        self.add_token(TokenType::EOF);
+    }
+
+    fn skip_whitespaces(&mut self) {
+        while !self.at_end() && self.current_char().is_whitespace() {
+            self.advance()
+        }
+    }
+
+    fn number(&mut self) {
+        self.advance();
+        while !self.at_end() && self.current_char().is_ascii_digit() {
+            self.advance();
+        }
+        self.add_token(TokenType::Number(self.lexeme().parse().unwrap()));
+    }
+
+    fn add_token(&mut self, tt: TokenType) {
+        self.tokens.push(Token::new(tt, self.lexeme()));
+        self.start = self.current
+    }
+
+    // self.currentの手前の文字までを切り出すことに注意
+    fn lexeme(&self) -> String {
+        self.src[self.start..self.current].iter().collect()
+    }
+
+    fn advance(&mut self) {
+        self.current += 1;
+    }
+
+    fn at_end(&self) -> bool {
+        self.current >= self.src.len()
+    }
+
+    fn current_char(&self) -> char {
+        self.src[self.current]
+    }
+}
+
+fn run(src: String) {
+    let mut scanner = Scanner::new(src);
+    scanner.scan();
+    if scanner.has_error {
+        return;
+    }
+
+    match scanner.tokens[0].tt {
+        TokenType::Number(_) => (),
+        _ => {
+            eprintln!("Error: number expected.");
+            return;
+        }
+    }
+    match scanner.tokens[1].tt {
+        TokenType::Semicolon => (),
+        _ => {
+            eprintln!("Error: semicolon expected.");
+            return;
+        }
+    }
+
+    match scanner.tokens[0].tt {
+        TokenType::Number(value) => {
+            println!("{}", value);
+        }
+        _ => (),
+    }
+}
+
 fn main() {
-    let buf = io::read_to_string(io::stdin()).expect("Error: failed to read the code.");
-    let src: Vec<char> = buf.chars().collect();
-
-    if src.len() < 2 {
-        panic!("Error: unexpected end of file.");
-    }
-    let num = src[0].to_digit(10).expect("Error: number expected.");
-    if src[1] != ';' {
-        panic!("Error: semicolon expected.")
-    }
-
-    println!("{num}");
+    let src = io::read_to_string(io::stdin()).expect("Error: failed to read the code.");
+    run(src);
 }
