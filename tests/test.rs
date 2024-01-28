4933dcc8d78224a0f00e738ccfb752d831a14f72
Kobayashi Takahiro <koba925@gmail.com>
2024-01-28 18:21:16 +0900

    2行目でエラーの出るテストケースを作る

--- tests/test.rs
+++ tests/test.rs
@@ -30,6 +30,11 @@ mod scanner {
     fn unexpected_character() {
         run::err1("@", 1, "Unexpected character ('@').");
     }
+
+    #[test]
+    fn unexpected_character_at_line_2() {
+        run::err1("\n@", 2, "Unexpected character ('@').");
+    }
 }
 
 mod expression {
