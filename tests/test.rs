c60a2a3d72e905bf9541f58532062e9752588aac
Kobayashi Takahiro <koba925@gmail.com>
2024-02-03 12:56:22 +0900

    テストを整理

--- tests/test.rs
+++ tests/test.rs
@@ -38,90 +38,104 @@ mod scanner {
 }
 
 mod expression {
-    use crate::run;
-    use jasc::ast::Value;
-    use jasc::error::Error;
 
-    #[test]
-    fn simple_number() {
-        run::ok("123;", Value::Number(123.0))
+    mod number {
+        use crate::run;
+        use jasc::ast::Value;
+
+        #[test]
+        fn simple_number() {
+            run::ok("123;", Value::Number(123.0))
+        }
+
+        #[test]
+        fn number_no_semicolon() {
+            run::err1("123", 1, "end", "Semicolon expected.");
+        }
     }
 
-    #[test]
-    fn add_12_34() {
-        run::ok("12+34;", Value::Number(46.0))
+    mod unary {
+        use crate::run;
+        use jasc::ast::Value;
+        use jasc::error::Error;
+
+        #[test]
+        fn unary_minus() {
+            run::ok("-12;", Value::Number(-12.0))
+        }
+
+        #[test]
+        fn add_unary_minus() {
+            run::ok("34+-12;", Value::Number(22.0))
+        }
+
+        #[test]
+        fn unary_minus_missing_right() {
+            run::err(
+                "-;",
+                vec![
+                    Error::new(1, "end", "Number expected."),
+                    Error::new(1, "end", "Semicolon expected."),
+                ],
+            );
+        }
     }
 
-    #[test]
-    fn add_12_34_56() {
-        run::ok("12+34+56;", Value::Number(102.0))
-    }
+    mod term {
+        use crate::run;
+        use jasc::ast::Value;
+        use jasc::error::Error;
 
-    #[test]
-    fn sub_34_12() {
-        run::ok("34-12;", Value::Number(22.0))
-    }
+        #[test]
+        fn add_12_34() {
+            run::ok("12+34;", Value::Number(46.0))
+        }
 
-    #[test]
-    fn sub_56_34_12() {
-        run::ok("56-34-12;", Value::Number(10.0))
-    }
+        #[test]
+        fn add_12_34_56() {
+            run::ok("12+34+56;", Value::Number(102.0))
+        }
 
-    #[test]
-    fn add_missing_right() {
-        run::err(
-            "12+;",
-            vec![
-                Error::new(1, "end", "Number expected."),
-                Error::new(1, "end", "Semicolon expected."),
-            ],
-        );
-    }
+        #[test]
+        fn sub_34_12() {
+            run::ok("34-12;", Value::Number(22.0))
+        }
 
-    #[test]
-    fn add_missing_right_after_2() {
-        run::err(
-            "12+34+;",
-            vec![
-                Error::new(1, "end", "Number expected."),
-                Error::new(1, "end", "Semicolon expected."),
-            ],
-        );
-    }
+        #[test]
+        fn sub_56_34_12() {
+            run::ok("56-34-12;", Value::Number(10.0))
+        }
 
-    #[test]
-    fn unary_minus() {
-        run::ok("-12;", Value::Number(-12.0))
-    }
+        #[test]
+        fn addition_no_semicolon() {
+            run::err1("12+34", 1, "end", "Semicolon expected.");
+        }
 
-    #[test]
-    fn unary_minus_missing_right() {
-        run::err(
-            "-;",
-            vec![
-                Error::new(1, "end", "Number expected."),
-                Error::new(1, "end", "Semicolon expected."),
-            ],
-        );
-    }
+        #[test]
+        fn addition_no_semicolon_after_3() {
+            run::err1("12+34+56", 1, "end", "Semicolon expected.");
+        }
 
-    #[test]
-    fn add_unary_minus() {
-        run::ok("34+-12;", Value::Number(22.0))
-    }
+        #[test]
+        fn add_missing_right() {
+            run::err(
+                "12+;",
+                vec![
+                    Error::new(1, "end", "Number expected."),
+                    Error::new(1, "end", "Semicolon expected."),
+                ],
+            );
+        }
 
-    #[test]
-    fn number_no_semicolon() {
-        run::err1("123", 1, "end", "Semicolon expected.");
-    }
-
-    #[test]
-    fn addition_no_semicolon() {
-        run::err1("12+34", 1, "end", "Semicolon expected.");
-    }
-
-    #[test]
-    fn addition_no_semicolon_after_3() {
-        run::err1("12+34+56", 1, "end", "Semicolon expected.");
+        #[test]
+        fn add_missing_right_after_2() {
+            run::err(
+                "12+34+;",
+                vec![
+                    Error::new(1, "end", "Number expected."),
+                    Error::new(1, "end", "Semicolon expected."),
+                ],
+            );
+        }
     }
 }
